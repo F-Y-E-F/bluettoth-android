c96230aacf0835d8e4b5fd0f30fc2e83b2863029
andryue <kacperwojak17@gmail.com>
2020-08-21 15:27:51 +0200

    add images and make layout details

--- app/src/main/java/com/example/tictactoearduino/PlayGame.kt
+++ app/src/main/java/com/example/tictactoearduino/PlayGame.kt
@@ -1,11 +1,28 @@
 package com.example.tictactoearduino
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
+import android.icu.lang.UProperty.INT_START
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_play_game.*
+
 
 class PlayGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_game)
+        val str = SpannableStringBuilder("It's  x  move")
+        str.setSpan(
+            StyleSpan(R.font.segoeuib),
+            4,
+            8,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        playerMoveText.text = str
     }
 }
