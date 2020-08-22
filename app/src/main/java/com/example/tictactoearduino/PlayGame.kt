477383a371cf81f1d1a7639a7276771f636cb74f
andryue <kacperwojak17@gmail.com>
2020-08-22 09:57:21 +0200

    find winner and show it on snackbar

--- app/src/main/java/com/example/tictactoearduino/PlayGame.kt
+++ app/src/main/java/com/example/tictactoearduino/PlayGame.kt
@@ -1,17 +1,14 @@
 package com.example.tictactoearduino
 
-import android.graphics.Typeface
-import android.icu.lang.UProperty.INT_START
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.StyleSpan
 import android.util.Log
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_play_game.*
 import kotlin.random.Random
 
@@ -36,6 +33,7 @@ class PlayGame : AppCompatActivity() {
 
     private fun setRandomStartPlayer(){
         activePlayer = if (Random.nextInt(0,1) == 0) "o" else "x"
+        setActivePlayerText(activePlayer)
     }
 
 
@@ -59,22 +57,59 @@ class PlayGame : AppCompatActivity() {
     }
 
     private fun checkWinner() {
-        var winner:Int = -1
-
-
-        for(i:Int in 1..3){
-                
-        }
-
+        showWinner(findWinner())
 
         Log.d("TAG",listOfOFields.toString())
         Log.d("TAG",listOfXFields.toString())
     }
+
+    private fun playerContains(firstField:Int,secondField:Int,thirdField:Int): ArrayList<Boolean>  =  arrayListOf(
+            listOfOFields.contains(firstField) && listOfOFields.contains(secondField) && listOfOFields.contains(thirdField),
+            listOfXFields.contains(firstField) && listOfXFields.contains(secondField) && listOfXFields.contains(thirdField))
+
 
 
     private fun setActivePlayerText(playerSymbol:String){
         val str = SpannableStringBuilder("It's $playerSymbol move")
         str.setSpan(StyleSpan(R.font.segoeuib), 4, 8, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         playerMoveText.text = str
+    }
+
+    private fun findWinner():Int{
+        var winner:Int = -1
+
+        when {
+            playerContains(0,1,2)[0] -> winner = 1
+            playerContains(0,1,2)[1] -> winner = 2
+            playerContains(3,4,5)[0] -> winner = 1
+            playerContains(3,4,5)[1] -> winner = 2
+            playerContains(6,7,8)[0] -> winner = 1
+            playerContains(6,7,8)[1] -> winner = 2
+            playerContains(0,3,6)[0] -> winner = 1
+            playerContains(0,3,6)[1] -> winner = 2
+            playerContains(1,4,7)[0] -> winner = 1
+            playerContains(1,4,7)[1] -> winner = 2
+            playerContains(2,5,8)[0] -> winner = 1
+            playerContains(2,5,8)[1] -> winner = 2
+            playerContains(0,4,8)[0] -> winner = 1
+            playerContains(0,4,8)[1] -> winner = 2
+            playerContains(2,4,6)[0] -> winner = 1
+            playerContains(2,4,6)[1] -> winner = 2
+            listOfOFields.size + listOfXFields.size == 9 -> winner = 0
+        }
+
+        return winner
+    }
+
+    private fun showWinner(winner:Int){
+        if(winner!=-1) listOfButtons.forEach {
+            it.isEnabled = false
+        }
+        when(winner){
+            0-> Snackbar.make(mainView,"Draw",Snackbar.LENGTH_LONG).setTextColor(Color.parseColor("#ffd500")).show()
+            1-> Snackbar.make(mainView,"o player win!",Snackbar.LENGTH_LONG).setTextColor(Color.parseColor("#ffd500")).show()
+            2-> Snackbar.make(mainView,"x player win!",Snackbar.LENGTH_LONG).setTextColor(Color.parseColor("#ffd500")).show()
+        }
+
     }
 }
